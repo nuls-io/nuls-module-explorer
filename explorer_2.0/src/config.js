a36304ba5d7de74d86fdcd7313f2a17de8b62eff
lijun.zhou <zhouj.172@gmail.com>
2019-07-04 21:09:55 +0800

    ui

--- explorer_2.0/src/config.js
+++ explorer_2.0/src/config.js
@@ -2,6 +2,6 @@ const IS_DEV = process.env.NODE_ENV !== 'production';
 //运行环境（true:正式环境，false:测试环境）
 export const RUN_DEV = false;
 //Request url
-export const API_ROOT = IS_DEV ? 'http://192.168.1.192:18003/' : 'https://api.nuls.io/';
+export const API_ROOT = IS_DEV ? 'http://192.168.1.192:18003/' : '/api/';
 //Request response time
 export const API_TIME = IS_DEV ? '9000' : '8000';
