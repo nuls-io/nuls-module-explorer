d444f959070ef3ef72646e0f2a741aebb6a88246
lijun.zhou <zhouj.172@gmail.com>
2019-07-08 19:13:22 +0800

    ui

--- explorer_2.0/src/App.vue
+++ explorer_2.0/src/App.vue
@@ -31,10 +31,11 @@ export default {
       const params = {"jsonrpc": "2.0", "method": "getChainInfo", "params": [], "id": 5898};
       axios.post(API_ROOT, params)
         .then((response) => {
-          //console.log(response);
+          console.log(response);
           if (response.hasOwnProperty("result")) {
             sessionStorage.setItem("chainId",response.result.chainId)
           }else {
+            console.log(response.hasOwnProperty("result"))
             sessionStorage.setItem("chainId","2")
           }
         })
