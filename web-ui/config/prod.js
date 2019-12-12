d06f5a928104905a6623b25627f06485d9648654
wave <986887991@qq.com>
2019-12-12 18:10:06 +0800

    prod

--- web-ui/config/prod.js
+++ web-ui/config/prod.js
@@ -8,7 +8,7 @@ export const IS_DEV = process.env.NODE_ENV === 'production';
 //运行环境
 export const RUN_DEV = true;
 //Request url
-export const API_ROOT = IS_DEV ? '/api' : 'https://public1.nuls.io';
+export const API_ROOT = IS_DEV ? '/api' : 'http://wallet.bbai.io:8004';
 //合约认证地址
 export const CODE_URL = 'https://nulscan.io/validation_contract';
 //Request response time
