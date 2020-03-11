479d813316ed392d80d38fed2bef68ce689401eb
lijunzhou <zhouj.172@gmail.com>
2020-03-11 09:56:50 +0800

    Update prod.js

--- web-ui/config/prod.js
+++ web-ui/config/prod.js
@@ -8,7 +8,7 @@ export const IS_DEV = process.env.NODE_ENV === 'production';
 //运行环境
 export const RUN_DEV = true;
 //Request url
-export const API_ROOT = IS_DEV ? '/api' : 'http://explorer.yuzhan.group/api';
+export const API_ROOT = IS_DEV ? '/api' : 'https://explorer.yuzhan.group/api';
 //合约认证地址
 export const CODE_URL = 'https://explorer.yuzhan.group/validation_contract';
 //Request response time
